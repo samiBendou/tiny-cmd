d05f672de8af9e324cd6e4610582130aca5cdde5
sami <sami@dahoux.net>
2020-08-21 12:14:31 +0200

    code: minimizing max sizes

--- include/cmd/consts.h
+++ include/cmd/consts.h
@@ -5,9 +5,9 @@
 #ifndef TINY_CMD_CONSTS_H
 #define TINY_CMD_CONSTS_H
 
-#define CMD_MAX_CMD 32
-#define CMD_LINE_LEN 512  /** maximum size of the line buffer */
-#define CMD_DESCR_LEN 256
+#define CMD_MAX_CMD 16
+#define CMD_LINE_LEN 128  /** maximum size of the line buffer */
+#define CMD_DESCR_LEN 128
 #define CMD_MAX_OPTS 10 /** maximum number of options in a command */
 #define CMD_MAX_EXCL 10
 #define CMD_NULL_OPT 0  /** label code for the null option */
